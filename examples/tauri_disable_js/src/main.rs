a0348cd63f4145f5cbe4ae4ccdd9b21d922f6078
Simon Laux <gpg@simonlaux.de>
2025-02-23 15:27:38 +0100

    local tauri example

--- examples/tauri_disable_js/src/main.rs
+++ examples/tauri_disable_js/src/main.rs
@@ -1,3 +1,31 @@
+// Copyright 2019-2024 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{LogicalPosition, LogicalSize, WebviewUrl};
+
 fn main() {
-    println!("Hello, world!");
+    tauri::Builder::default()
+        .setup(|app| {
+            let width = 800.;
+            let height = 600.;
+
+            let window = tauri::window::WindowBuilder::new(app, "main")
+                .inner_size(width, height)
+                .build()?;
+
+            let _webview1 = window.add_child(
+                tauri::webview::WebviewBuilder::new("main1", WebviewUrl::App(Default::default()))
+                    .disable_javascript()
+                    .auto_resize(),
+                LogicalPosition::new(0., 0.),
+                LogicalSize::new(width, height),
+            )?;
+
+            Ok(())
+        })
+        .run(tauri::generate_context!("./tauri.conf.json"))
+        .expect("error while running tauri application");
 }
